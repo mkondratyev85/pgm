bc11a0406e84a0ff3ecc2907ebec6ca55f07fac6
Mikhail Kondratyev <fatune@gmail.com>
2015-11-12 13:24:10 +1200

    Saving of arrayis in the same folder. Loading arrays

--- pgm.py
+++ pgm.py
@@ -3,23 +3,37 @@ import matplotlib.pylab as plt
 from scipy import sparse
 from Stokes import return_sparse_matrix_Stokes
 from interpolate import interpolate, interpolate2m
+import subprocess
 
 from test_mmodel import load_model_simple
 
+def load_step(filename, Step):
+	data = np.load("%s/%s.npz")
+	mxx = data['mxx']
+	myy = data['myy']
+	m_cat = data['m_cat']
+	m_eta = data['m_eta']
+	m_rho = data['m_rho']
+
+	width, height, j_res, i_res, gx_0, gy_0 = np.loadtxt("%s/params.txt", unpack=True)
+
+	return width, height, j_res, i_res, gx_0, gy_0, mxx, myy, m_cat, m_rho, m_eta
+
 class PGM:
-	def __init__(self, (width, height), (j_res, i_res), (gx_0, gy_0), (mxx, myy, m_cat, m_rho, m_eta)):
+	def __init__(self, width, height, j_res, i_res, gx_0, gy_0, mxx, myy, m_cat, m_rho, m_eta):
 		self.height, self.width  = width, height
-		self.j_res, self.j_res = j_res, i_res
+		self.j_res, self.i_res = j_res, i_res
 		self.dx = width / (j_res-1)
 		self.dy = height/ (i_res-1)
-		self.dx2, self.dy2 = dx**2, dy**2
+		self.dx2, self.dy2 = self.dx**2, self.dy**2
 
+		self.gx_0, self.gy_0 = gx_0, gy_0
 		self.p0cell  =  0 # Pressure condition in one cell (i==2 && j==3)
 
 		# define grid for messing with indexes
 		self.j      = np.linspace(0,j_res-1,j_res).astype('int')
 		self.i      = np.linspace(0,i_res-1,i_res).astype('int')
-		self.jj,self.ii  = np.meshgrid(j,i)
+		self.jj,self.ii  = np.meshgrid(self.j,self.i)
 		
 		self.mxx = mxx
 		self.myy = myy
@@ -27,12 +41,29 @@ class PGM:
 		self.m_rho = m_rho
 		self.m_eta = m_eta
 
-		self.kbond   = 4*m_eta.min()/((dx+dy)**2)
-		self.kcont   = 2*m_eta.min()/(dx+dy)
+		self.kbond   = 4*self.m_eta.min()/((self.dx+self.dy)**2)
+		self.kcont   = 2*self.m_eta.min()/(self.dx+self.dy)
+		self.label = subprocess.check_output(["git", "describe", "--always"])[:-1].decode('utf-8')
 
-	def __run__(self, maxT, step, filename):
+	def run(self, maxT, step, filename):
+		mxx = self.mxx 
+		myy = self.myy 
+		m_cat = self.m_cat
+		m_rho = self.m_rho
+		m_eta = self.m_eta
+		
+		i_res, j_res = self.i_res, self.j_res
+		dx, dy = self.dx, self.dy
+		gx_0, gy_0 = self.gx_0, self.gy_0
+		kbond, kcont = self.kbond, self.kcont
+		p0cell = self.p0cell
+
+		self.figname = filename
+
 		T = 0
 		Step = -1
+
+		np.savetxt("%s/params.txt" % self.figname, np.vstack((self.width, self.height, j_res, i_res, gx_0, gy_0)).T)
 
 		while T < maxT:
 			# we should interpolate eta_n separately but actually eta_n and eta_s are equal
@@ -85,22 +116,24 @@ class PGM:
 			s_xy_ = 2 * eta_s[1:-1,1:-1] * e_xy_
 
 			s_xx_ = (s_xx[:-1,:-1] + s_xx[:-1,1:] + s_xx[1:,1:] + s_xx[1:,:-1]) / 4
+			e_xx_ = (e_xx[:-1,:-1] + e_xx[:-1,1:] + e_xx[1:,1:] + e_xx[1:,:-1]) / 4
 
 			sii = (s_xx_**2 + s_xy_**2)**.5
 
 			Vx_average = 0.5*(Vx[1:-1,:-2]+Vx[:-2,:-2])
 			Vy_average = 0.5*(Vy[ :-2,1:-1]+Vy[:-2,:-2])
 			
-			self.plot(T, eta_n, mxx, myy, sii, P, Vx_average, Vy_average)
+			self.plot(T, Step, eta_n, mxx, myy, m_cat, sii, P, Vx_average, Vy_average)
+			self.save(Step, mxx, myy, m_cat, m_eta, m_rho)
 
-	def plot(self):
+	def plot(self,T, Step, eta_n, mxx, myy, m_cat, sii, P, Vx_average, Vy_average):
 		Myr = lambda t: t/(365.25*24*3600*10**6) # Convert seconds to millions of year
 
 		plt.clf()
 		fig = plt.figure(figsize=(15,10))
 
-		plt.suptitle("Model size: %s km x %s km (%s x %s cells, dx=%s km, dy=%s km). Current Time: %07.3f Myr. Step %s" %
-					(self.width/1000, self.height/1000, self.j_res, self.i_res, self.dx/1000, self.dy/1000, Myr(T), Step))
+		plt.suptitle("Model size: %s km x %s km (%s x %s cells, dx=%s km, dy=%s km). Current Time: %07.3f Myr. Step %s git verstion: %s" %
+					(self.width/1000, self.height/1000, self.j_res, self.i_res, self.dx/1000, self.dy/1000, Myr(T), Step, self.label))
 		plt.subplot(2,2,1)
 		plt.title("Viscosity")
 		plt.imshow(eta_n,interpolation='none',cmap='copper')
@@ -109,22 +142,27 @@ class PGM:
 		plt.subplot(2,2,2)
 		plt.scatter(mxx,myy,c=m_cat,s=1,edgecolors='face',cmap='copper')
 		plt.colorbar()
-		plt.ylim([i_res-1,0])
-		plt.xlim([0,j_res-1])
+		plt.ylim([self.i_res-1,0])
+		plt.xlim([0,self.j_res-1])
 		
 		plt.subplot(2,2,3)
 		plt.title("Sigma II")
-		plt.imshow(sii[:-1,:-1],interpolation='none',norm=LogNorm())
+		plt.imshow(sii[:-1,:-1],interpolation='none')
 		plt.colorbar()
 
 		plt.subplot(2,2,4)
 		plt.title("P")
 		plt.imshow(P[1:,1:],interpolation='none')
 		plt.colorbar()
-		plt.streamplot(jj[:-2,:-2],ii[:-2,:-2],Vx_average,Vy_average,color='white')
-		plt.ylim([i_res-2,0])
-		plt.xlim([0,j_res-2])
-		
+		plt.streamplot(self.jj[:-2,:-2],self.ii[:-2,:-2],Vx_average,Vy_average,color='white')
+		plt.ylim([self.i_res-2,0])
+		plt.xlim([0,self.j_res-2])
+
 		plt.savefig('%s/%08.4f.png' % (self.figname, Myr(T)))
 		plt.close(fig)
 
+	
+	def save(self, Step, mxx, myy, m_cat, m_eta, m_rho):
+		Myr = lambda t: t/(365.25*24*3600*10**6) # Convert seconds to millions of year
+		array = np.vstack((mxx,myy,m_cat, m_eta,m_rho)).T
+		np.savez("%s/%s.npz" % (self.figname, Step), mxx, myy, m_cat, m_eta, m_rho)
