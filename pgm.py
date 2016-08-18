3fe7f675666bffdc6b79312913ac27a8a4713465
mikhail <fatune@gmail.com>
2016-08-18 16:08:38 +1200

    Working version of elvisgit add benchmark.py m2.pnggit add benchmark.py m2.pnggit add benchmark.py m2.png

--- pgm.py
+++ pgm.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 from scipy import sparse
-#from Stokes import return_sparse_matrix_Stokes
-from Stokeselvis_new import return_sparse_matrix_Stokes
+from Stokeselvis import return_sparse_matrix_Stokes
 from interpolate import interpolate, interpolate2m, interpolate_harmonic
 import subprocess
 
@@ -95,14 +94,22 @@ class PGM:
 		m_mu = self.m_mu
 		m_C = self.m_C
 		m_sinphi = self.m_sinphi
+		m_s_ii_old = np.zeros(np.shape(mxx))
 		m_s_xx = np.zeros(np.shape(mxx))
 		m_s_xy = np.zeros(np.shape(mxx))
+		m_e_ii_old = np.zeros(np.shape(mxx))
+		m_e_xx = np.zeros(np.shape(mxx))
+		m_e_xy = np.zeros(np.shape(mxx))
+		m_P    = np.zeros(np.shape(mxx))
 		
 		i_res, j_res = self.i_res, self.j_res
 		dx, dy = self.dx, self.dy
 		gx_0, gy_0 = self.gx_0, self.gy_0
 		kbond, kcont = self.kbond, self.kcont
 		p0cell = self.p0cell
+
+		eta_min = 1e+10
+		eta_max = 1e+24
 
 		T = self.T
 		Step =  self.Step
@@ -127,110 +134,149 @@ class PGM:
 		if not T : T = 0
 		if not Step: Step = -1
 
-		print(self.left_bound)
 		np.savetxt("%s/params.txt" % self.figname,
 				        np.vstack((self.width, self.height, j_res, i_res, gx_0, gy_0,
 				        right_bound, left_bound, top_bound,bottom_bound)).T)
 
 		dt = 0
+		plastic_iterations = 1
 		while T < maxT:
-			print(dt)
 			dt = max(dt, dt_min)
+			for i in range(plastic_iterations):
+				plastic_yield = False
 
-			# we should interpolate eta_n separately but actually eta_n and eta_s are equal
-			eta, rho, sxx_0, sxy_0 = interpolate(mxx,myy,i_res,j_res, (m_eta, m_rho, m_s_xx, m_s_xy))
-			mu = interpolate_harmonic(mxx,myy,i_res,j_res, m_mu )
+				m_xelvis = m_eta/(m_mu*dt + m_eta)
+				m_s_xx_new = m_s_xx*m_xelvis + 2*m_eta*m_e_xx*(1-m_xelvis)
+				m_s_xy_new = m_s_xy*m_xelvis + 2*m_eta*m_e_xy*(1-m_xelvis)
 
-			#Check if we have nans
-			if np.isnan(eta).any(): fill_nans(eta)
-			if np.isnan(rho).any(): fill_nans(rho)
-			if np.isnan(mu).any(): fill_nans(mu)
-			if np.isnan(sxx_0).any(): fill_nans(sxx_0)
-			if np.isnan(sxy_0).any(): fill_nans(sxy_0)
+				m_s_ii_new = (m_s_xx_new**2 + m_s_xy_new**2)**.5
+				m_s_ii_yield = m_C + m_sinphi*m_P
+				mask = m_s_ii_yield <0
+				m_s_ii_yield[mask] = 0
 
-			Z = dt*mu/(dt*mu + eta)
-			eta = eta * Z
-			sxx_0 = sxx_0 * (1 - Z)
-			sxy_0 = sxy_0 * (1 - Z)
+				ym = m_s_ii_yield < m_s_ii_new # yielding mask
+				if not np.all(ym):
+					plastic_yield = True
 
-			Stokes_sparse, vector = return_sparse_matrix_Stokes(j_res, i_res, dx, dy, 
-					eta, eta, rho, gx_0, gy_0, sxx_0, sxy_0, kbond, kcont, p0cell, 
-					lower_boundary=self.bottom_bound, upper_boundary=self.top_bound,
-					right_boundary=self.right_bound, left_boundary=self.left_bound)
+					m_e_ii_old[ym] = np.sqrt(m_e_xx[ym]**2 + m_e_xy[ym]**2)
+					m_eta[ym]      = m_s_ii_yield[ym] /2/m_e_ii_old[ym]
+					m_s_ii_old[ym] = np.sqrt(m_s_xx[ym]**2 + m_s_xy[ym]**2)
+					m_s_xx[ym]     = m_s_xx[ym]*m_s_ii_yield[ym]/m_s_ii_old[ym]
+					m_s_xy[ym]     = m_s_xy[ym]*m_s_ii_yield[ym]/m_s_ii_old[ym]
 
-			Stokes_solve = sparse.linalg.spsolve(Stokes_sparse, vector)
-			P  = Stokes_solve[::3].reshape((i_res),(j_res))
-			Vx = Stokes_solve[1::3].reshape((i_res),(j_res))
-			Vy = Stokes_solve[2::3].reshape((i_res),(j_res))
+					# eta_min < eta < eta_max
+					m_eta[m_eta<eta_min] = eta_min
+					m_eta[m_eta>eta_max] = eta_max
 
-			P *= kcont
-			Vx *= kcont
-			Vy *= kcont
+				# we should interpolate eta_n separately but actually eta_n and eta_s are equal
+				eta, rho, so_xx, so_xy = interpolate(mxx,myy,i_res,j_res, (m_eta, m_rho, m_s_xx, m_s_xy))
+				mu = interpolate_harmonic(mxx,myy,i_res,j_res, m_mu )
+
+				#Check if we have nans
+				if np.isnan(eta).any(): fill_nans(eta)
+				if np.isnan(rho).any(): fill_nans(rho)
+				if np.isnan(mu).any(): fill_nans(mu)
+				if np.isnan(so_xx).any(): fill_nans(so_xx)
+				if np.isnan(so_xy).any(): fill_nans(so_xy)
+
+				#Z = dt*mu/(dt*mu + eta)
+				#eta = eta * Z
+				#sxx_0 = sxx_0 * (1 - Z)
+				#sxy_0 = sxy_0 * (1 - Z)
+
+				# compute viscoelastic (numerical) viscosity and stress
+				eta0 = eta
+				so_xy0 = so_xy
+				so_xx0 = so_xx
+				xelvis = eta/(eta+dt*mu)
+				eta = eta*(1-xelvis)
+				so_xy = so_xy*xelvis
+				so_xx = so_xx*xelvis
+
+				Stokes_sparse, vector = return_sparse_matrix_Stokes(j_res, i_res, dx, dy, 
+						eta, eta, rho, gx_0, gy_0, so_xx, so_xy, kbond, kcont, p0cell, 
+						lower_boundary=self.bottom_bound, upper_boundary=self.top_bound,
+						right_boundary=self.right_bound, left_boundary=self.left_bound)
+
+				Stokes_solve = sparse.linalg.spsolve(Stokes_sparse, vector)
+				P  = Stokes_solve[::3].reshape((i_res),(j_res))
+				Vx = Stokes_solve[1::3].reshape((i_res),(j_res))
+				Vy = Stokes_solve[2::3].reshape((i_res),(j_res))
+
+				P *= kcont
+				#Vx *= kcont
+				#Vy *= kcont
 
 
-			Vx_max = np.abs(Vx).max()
-			Vy_max = np.abs(Vy).max()
-			dtx = 0.5 * dx / Vx_max 
-			dty = 0.5 * dy / Vy_max 
-			dt = min(dtx,dty)
+				Vx_max = np.abs(Vx).max()
+				Vy_max = np.abs(Vy).max()
+				dtx = 0.1 * dx / Vx_max 
+				dty = 0.1 * dy / Vy_max 
+				dt = min(dtx,dty)
 
-			m_Vx = interpolate2m(mxx   , myy-.5, Vx[:-1,:])
-			m_Vy = interpolate2m(mxx-.5, myy   , Vy[:,:-1])
-			m_P  = interpolate2m(mxx-.5, myy-.5, P[1:,1:]) # tecnichaly, there must be +.5,+.5 but since we slice P, indexing goes one item lower
+				#dVx, dVy   = Vx[1:,  :-1] - Vx[:-1, :-1], Vy[ :-1, 1:] - Vy[:-1, :-1]
+				#dVx_, dVy_ = Vx[ :, 1:-1] - Vx[:,   :-2], Vy[1:-1,  :] - Vy[:-2, :]
+				dVx_dx = (Vx[:-1,1:] - Vx[:-1,:-1])/dx
+				dVy_dy = (Vy[1:,:-1] - Vy[:-1,:-1])/dy
 
+				dVx_dy = (Vx[1:-1,:] - Vx[:-2,:])/dy
+				dVy_dx = (Vy[:,1:-1] - Vy[:,:-2])/dx
 
-			#dVx, dVy   = Vx[1:,  :-1] - Vx[:-1, :-1], Vy[ :-1, 1:] - Vy[:-1, :-1]
-			#dVx_, dVy_ = Vx[ :, 1:-1] - Vx[:,   :-2], Vy[1:-1,  :] - Vy[:-2, :]
-			dVx_dx = (Vx[:-1,1:] - Vx[:-1,:-1])/dx
-			dVy_dy = (Vy[1:,:-1] - Vy[:-1,:-1])/dy
+				#concatenating array to add additional rows and columns. Just copy remaning columns
+				#        abc    ad    aadd
+				# abc -> abc    be -> bbee
+				# def    def    cf    ccff
+				#        def
+				dVx_dy = np.append(dVx_dy[0,:][None,:],dVx_dy,axis=0)
+				dVx_dy = np.append(dVx_dy,dVx_dy[-1,:][None,:],axis=0)
+				dVy_dx = np.append(dVy_dx[:,0][:,None],dVy_dx,axis=1)
+				dVy_dx = np.append(dVy_dx,dVy_dx[:,-1][:,None],axis=1)
 
-			dVx_dy = (Vx[1:-1,:] - Vx[:-2,:])/dy
-			dVy_dx = (Vy[:,1:-1] - Vy[:,:-2])/dx
+				e_xx = dVx_dx # strain rate
+				e_xy = .5 * (dVx_dy + dVy_dx)
+				s_xx = (1-average(xelvis))*2*average(eta0)*e_xx + average(xelvis)*average(so_xx0)
+				s_xy = (1-xelvis)*2*eta0*e_xy + xelvis*so_xy0
+				s_ii = (s_xx**2 + average(s_xy**2))**.5
 
-			#concatenating array to add additional rows and columns. Just copy remaning columns
-			#        abc    ad    aadd
-			# abc -> abc    be -> bbee
-			# def    def    cf    ccff
-			#        def
-			dVx_dy = np.append(dVx_dy[0,:][None,:],dVx_dy,axis=0)
-			dVx_dy = np.append(dVx_dy,dVx_dy[-1,:][None,:],axis=0)
-			dVy_dx = np.append(dVy_dx[:,0][:,None],dVy_dx,axis=1)
-			dVy_dx = np.append(dVy_dx,dVy_dx[:,-1][:,None],axis=1)
+				d_sxx = s_xx - average(so_xx0)
+				d_sxy = s_xy - so_xy0
 
-			e_xx = dVx_dx # strain rate
-			s_xx = 2 * eta[1:,1:] * e_xx
+				m_Vx = interpolate2m(mxx   , myy-.5, Vx[:-1,:])
+				m_Vy = interpolate2m(mxx-.5, myy   , Vy[:,:-1])
+				m_P  = interpolate2m(mxx-.5, myy-.5, P[1:,1:]) # tecnichaly, there must be +.5,+.5 but since we slice P, indexing goes one item lower
+				w = dVy_dx - dVx_dy
 
-			d_s_xx = s_xx - average(sxx_0)
+				m_s_xx = interpolate2m(mxx-.5,myy-.5,s_xx)
+				m_s_xy = interpolate2m(mxx,myy,s_xy)
 
-			e_xy = .5 * (dVx_dy + dVy_dx)
-			s_xy = 2 * eta * e_xy
+				m_w    = interpolate2m(mxx-.5 ,myy-.5 , w)
+				
+				m_a = m_w * dt
+				m_s_xx_ = m_s_xx - m_s_xy * 2 * m_a
+				m_s_xy_ = m_s_xy + m_s_xy * 2 * m_a
+				m_s_xx, m_s_xy = m_s_xx_, m_s_xy_
 
-			d_s_xy = s_xy - sxy_0
+				mxx += m_Vx*dt/dx
+				myy += m_Vy*dt/dy
 
-			sii = (s_xx**2 + average(s_xy)**2)**.5
-			eii = (e_xx**2 + average(e_xy)**2)**.5
+				T += dt
+				Step +=1
 
-			w = dVy_dx - dVx_dy
+			#s_xx = 2 * eta[1:,1:] * e_xx
 
-			m_s_xx = interpolate2m(mxx-.5,myy-.5,s_xx)
-			m_s_xy = interpolate2m(mxx,myy,s_xy)
+			#d_s_xx = s_xx - average(sxx_0)
 
-			m_w    = interpolate2m(mxx-.5 ,myy-.5 , w)
-			
-			m_a = m_w * dt
-			m_s_xx_ = m_s_xx - m_s_xy * 2 * m_a
-			m_s_xy_ = m_s_xy + m_s_xy * 2 * m_a
-			m_s_xx, m_s_xy = m_s_xx_, m_s_xy_
+			#s_xy = 2 * eta * e_xy
 
-			mxx += m_Vx*dt/dx
-			myy += m_Vy*dt/dy
+			#d_s_xy = s_xy - sxy_0
 
-			T += dt
-			Step +=1
+			#sii = (s_xx**2 + average(s_xy)**2)**.5
+			#eii = (e_xx**2 + average(e_xy)**2)**.5
+
 
 			if Step % step : continue
 
-			self.plot(T, Step, eta, mxx, myy, m_cat, sii, P, Vx, Vy)
+			self.plot(T, Step, eta, mxx, myy, m_cat, s_ii, P, Vx, Vy)
 			self.save(Step, mxx, myy, m_cat, m_eta, m_rho, m_C, m_sinphi)
 
 	def plot(self,T, Step, eta_n, mxx, myy, m_cat, sii, P, Vx, Vy):
@@ -259,12 +305,10 @@ class PGM:
 
 		Vx_average = 0.5*(Vx[1:-1,:-2]+Vx[:-2,:-2])
 		Vy_average = 0.5*(Vy[ :-2,1:-1]+Vy[:-2,:-2])
-		print(Vx.shape, Vy.shape)
 		plt.subplot(2,2,4)
 		plt.title("P")
 		plt.imshow(P[1:,1:],interpolation='none')
 		plt.colorbar()
-		print(self.jj.shape,self.ii.shape,Vx_average.shape,Vy_average.shape)
 		#plt.streamplot(self.jj[:-2,:-2],self.ii[:-2,:-2],Vx_average,Vy_average,color='white')
 		plt.ylim([self.i_res-2,0])
 		plt.xlim([0,self.j_res-2])
