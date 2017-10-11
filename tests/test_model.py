9d4e0255a849fd9baf310698ffc9abde9004a231
mikhail kondratyev <fatune@gmail.com>
2017-10-11 17:33:03 +1100

    Add tests directory

---
+++ tests/test_model.py
@@ -0,0 +1,12 @@
+import unittest
+
+from interpolate import interpolate2m
+
+class TestMode(unittest.TestCase):
+
+    def test_interpolation(self):
+
+        pass
+
+if __name__=='__main__':
+    unittest.main()
