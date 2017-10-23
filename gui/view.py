2e46b61ee9852648f1983c66a59a784df65f2ccd
Mikhail.Kondratyev <fatune@gmail.com>
2017-10-24 09:38:19 +1100

    Add fileopen dialogs to gui/view.py

--- gui/view.py
+++ gui/view.py
@@ -1,5 +1,6 @@
 
-import matplotlib, sys
+import matplotlib
+import sys
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib import pylab as plt
@@ -7,6 +8,7 @@ import numpy as np
 
 import tkinter as Tk
 import tkinter.ttk as ttk
+
 
 class View(object):
     selected_category = None
@@ -22,10 +24,20 @@ class View(object):
 
         # create a pulldown menu, and add it to the menu bar
         filemenu = Tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Add image...", command=lambda: fadd(Tk.filedialog.askopenfilename()))
+        filemenu.add_command(label="Add image...", command=lambda: fadd(
+            Tk.filedialog.askopenfilename(title="Select image file",
+                                          filetypes=(("png files", "*.png"),
+                                                     ("numpy array files", "*.npy"),
+                                                     ("all files", "*.*")))))
         filemenu.add_separator()
-        filemenu.add_command(label="Load...", command=lambda: fload(Tk.filedialog.askopenfilename()))
-        filemenu.add_command(label="Save...", command=lambda: fsave(Tk.filedialog.asksaveasfilename()))
+        filemenu.add_command(label="Load...", command=lambda: fload(
+            Tk.filedialog.askopenfilename(title="Select model file",
+                                          filetypes=(("python files", "*.py"),
+                                                     ("all files", "*.*")))))
+        filemenu.add_command(label="Save...", command=lambda: fsave(
+            Tk.filedialog.asksaveasfilename(title="Select file to save a model",
+                                          filetypes=(("python files", "*.py"),
+                                                     ("all files", "*.*")))))
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.root.quit)
         menubar.add_cascade(label="File", menu=filemenu)
@@ -37,11 +49,10 @@ class View(object):
     # 	fname = Tk.filedialog.asksaveasfilename()
     #     self.fsave(fname)
 
-
     def main_loop(self):
         self.root.mainloop()
 
     def quit(self, *args):
-    	print ('quit button press...')
-    	self.root.quit()
-    	self.root.destroy()
+        print('quit button press...')
+        self.root.quit()
+        self.root.destroy()
