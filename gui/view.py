33bc217907ef10541c69d31a492767f2639c1eac
Mikhail.Kondratyev <fatune@gmail.com>
2017-10-24 11:12:32 +1100

    gui/view.py mark material on canvas with red when clicked lb

--- gui/view.py
+++ gui/view.py
@@ -1,4 +1,3 @@
-
 import matplotlib
 import sys
 matplotlib.use('TkAgg')
@@ -9,6 +8,7 @@ import numpy as np
 import tkinter as Tk
 import tkinter.ttk as ttk
 
+from .materials import materials as materials_
 
 class View(object):
     selected_category = None
@@ -69,12 +69,62 @@ class View(object):
         group.pack(side=Tk.RIGHT,fill=Tk.Y)
 
         # create list of materials
-        catgroup = Tk.LabelFrame(group, text="List of categories:")
+        catgroup = Tk.LabelFrame(group, text="List of model materials:")
         catgroup.pack(fill=Tk.X)
 
         self.lb_materials = Tk.Listbox(catgroup)
-        # self.lb_materials.bind("<<ListboxSelect>>", self.listbox_get2)
+        self.lb_materials.bind("<<ListboxSelect>>", self.lb_material_selected)
         self.lb_materials.pack(fill=Tk.X)
+
+        # create chose of selected material
+        propgroup = Tk.LabelFrame(group, text="Properties of selected material:")
+        propgroup.pack()
+
+        self.muvar = Tk.StringVar()
+        self.etavar = Tk.StringVar()
+        self.rhovar = Tk.StringVar()
+        self.Cvar = Tk.StringVar()
+        self.sinphivar = Tk.StringVar()
+
+        self.materialvar = Tk.StringVar()
+        self.materialvar.trace("w", self.material_selected)
+        material = ttk.Combobox(propgroup, textvariable=self.materialvar)
+        material['values'] = (list(materials_.keys()))
+        material.current(0)
+        material.pack()
+
+        mulabel = Tk.Label(propgroup,textvariable=self.muvar).pack()
+        etalabel = Tk.Label(propgroup,textvariable=self.etavar).pack()
+        rholabel = Tk.Label(propgroup,textvariable=self.rhovar).pack()
+        Clabel = Tk.Label(propgroup,textvariable=self.Cvar).pack()
+        sinphilabel = Tk.Label(propgroup,textvariable=self.sinphivar).pack()
+
+    def material_selected(self, *args):
+        if self.selected_category == None:
+            return False
+        selectedmaterial = self.materialvar.get()
+        self.materials[self.selected_category] = { "name":selectedmaterial,
+                                               "rho":materials_[selectedmaterial]["rho"],
+                                               "eta":materials_[selectedmaterial]["eta"],
+                                               "mu":materials_[selectedmaterial]["mu"],
+                                               "C":materials_[selectedmaterial]["C"],
+                                               "sinphi":materials_[selectedmaterial]["sinphi"], }
+        self.muvar.set("mu = %s" % self.materials[self.selected_category]["mu"])
+        self.rhovar.set("rho = %s" % self.materials[self.selected_category]["rho"])
+        self.etavar.set("eta = %s" % self.materials[self.selected_category]["eta"])
+        self.Cvar.set("C = %s" % self.materials[self.selected_category]["C"])
+        self.sinphivar.set("sinphi = %s" % self.materials[self.selected_category]["sinphi"])
+
+    def lb_material_selected(self, event):
+        try:
+            self.selected_category =  int(event.widget.curselection()[0])
+        except IndexError:
+            pass
+        # redraw
+        image_to_show = self.array.get().copy()
+        image_to_show[image_to_show == self.selected_category] = -1
+        self.redraw_canvas(image_to_show)
+
 
     def update_lb_materials(self, *args):
         listbox = self.lb_materials
@@ -91,8 +141,8 @@ class View(object):
         self.root.destroy()
 
     def redraw_canvas(self, im_to_show=None):
-        if im_to_show == None:
-            self.im = plt.imshow(self.array, cmap=self.my_cmap)
-        else:
+        if not im_to_show is None:
             self.im.set_data(im_to_show)
+        else:
+            self.im = plt.imshow(self.array, cmap=self.my_cmap)
         self.canvas.draw()
