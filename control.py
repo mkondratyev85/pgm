8b74ff4cb9b8c8711064d7cafb88348affa6e5fa
Mikhail.Kondratyev <fatune@gmail.com>
2017-10-25 14:18:37 +1100

    Add load function that loads model from pickled objects

--- control.py
+++ control.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 import matplotlib.pylab as plt
 from scipy import sparse
 import subprocess
@@ -72,3 +73,91 @@ class PGM:
                 if iteration['step'] % 10:
                     continue
                 self.view.plot12(iteration)
+
+
+def load_image(fname, i_res, j_res, marker_density):
+    image = np.load(f'{fname}.npy')
+    image_i, image_j = image.shape
+
+    # markers
+    mxx = []
+    myy = []
+    for x in range((j_res-1)*2):
+        for y in range((i_res-1)*2):
+            for _ in range(marker_density):
+                mxx.append((x+np.random.uniform(0,1,1))/2.0)
+                myy.append((y+np.random.uniform(0,1,1))/2.0)
+    mxx = np.asarray(mxx)
+    myy = np.asarray(myy)
+
+    mj = (mxx*image_j/(j_res-1)).astype(int)
+    mi = (myy*image_i/(i_res-1)).astype(int)
+
+    values = np.zeros(np.shape(mxx))
+    for idx in range(len(mxx)):
+        j,i = mj[idx], mi[idx]
+        values[idx] = image[i,j]
+
+    values = values.astype(int)
+
+    return mxx, myy, values
+
+def load_model_properties(fname):
+    with open(f'{fname}.pickle', 'rb') as f:
+        materials = pickle.load(f)
+        boundaries = pickle.load(f)
+        moving_cells = pickle.load(f)
+    return materials, boundaries, moving_cells
+
+def load_model(fname, i_res, j_res, pdensity):
+    mxx, myy, values = load_image(fname, i_res, j_res, pdensity)
+    materials, boundaries, moving_cells  = load_model_properties(fname)
+
+    rho_key = np.asarray([material['rho'] for  material in materials])
+    eta_key = np.asarray([material['eta'] for  material in materials])
+    mu_key = np.asarray([material['mu'] for  material in materials])
+    C_key = np.asarray([material['C'] for  material in materials])
+    sinphi_key = np.asarray([material['sinphi'] for  material in materials])
+
+    m_cat = np.copy(values)
+    m_rho = rho_key[values]
+    m_eta = eta_key[values]
+    m_mu = mu_key[values]
+    m_C = C_key[values]
+    m_sinphi = sinphi_key[values]
+
+    m_s_xx = np.zeros(np.shape(mxx))
+    m_s_xy = np.zeros(np.shape(mxx))
+    m_e_xx = np.zeros(np.shape(mxx))
+    m_e_xy = np.zeros(np.shape(mxx))
+    m_P    = np.zeros(np.shape(mxx))
+
+    print(boundaries)
+    top_bound = boundaries['top_bound']
+    bottom_bound = boundaries['bottom_bound']
+    left_bound = boundaries['left_bound']
+    right_bound = boundaries['right_bound']
+
+    model_prop = {
+                  "mxx": mxx,
+                  "myy": myy,
+                  "m_cat": m_cat,
+                  "m_rho": m_rho,
+                  "m_eta": m_eta,
+                  "m_mu": m_mu,
+                  "m_C": m_C,
+                  "m_sinphi": m_sinphi,
+
+                  "top_bound": top_bound,
+                  "bottom_bound": bottom_bound,
+                  "left_bound": left_bound,
+                  "right_bound": right_bound,
+
+                  "m_s_xx": m_s_xx,
+                  "m_s_xy": m_s_xy,
+                  "m_e_xx": m_e_xx,
+                  "m_e_xy": m_e_xy,
+                  "m_P": m_P,
+                  }
+
+    return model_prop
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from control import PGM
-from benchmark2 import load_model
+from control import PGM, load_model
+#from benchmark2 import load_model
 
 
 sec = lambda t: t * 365.25 * 24 * 3600 * 10**6 # Converts Myrs back to seconds
@@ -13,7 +13,9 @@ gy_0 = 10 # vertical gravity field in m/s2
 
 pdensity = 5
 
-model_prop = load_model(i_res, j_res, pdensity)
+# model_prop = load_model(i_res, j_res, pdensity)
+fname = '/tmp/t.py'
+model_prop = load_model(fname, i_res, j_res, pdensity)
 
 figname = '/tmp/t7/'
 control = PGM( width, height, j_res, i_res, gx_0, gy_0, model_prop, figname = figname)
