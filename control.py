b4a4c16b998530b85d2fe8e35be4311d02b82c11
mikhail kondratyev <fatune@gmail.com>
2017-10-27 21:49:43 +1100

    Refactor pgm.py

--- control.py
+++ control.py
@@ -9,12 +9,12 @@ from scipy import sparse
 
 from view import Matplot
 from model import Model
+from largetime import Time
 
 
 average = lambda x: (x[:-1,:-1] + x[1:,:-1] +x[1:,1:] +x[:-1,1:])/4.0
 
 class PGM:
-    #def __init__(self, width, height, j_res, i_res, gx_0, gy_0, model_prop, T = None, Step = None, p0cell=0,figname=''):
     def __init__(self, model_prop, T = None, Step = None, figname=''):
         self.height, self.width  = int(model_prop['width']), int(model_prop['height'])
         self.j_res, self.i_res = int(model_prop['j_res']), int(model_prop['i_res'])
@@ -65,12 +65,12 @@ class PGM:
                             git = self.label,
                             figname = self.figname )
 
-    def init_(self, parameters):
-        self.model = Model(parameters)
+        self.model = Model(model_prop)
+        self.maxT = Time(model_prop['MaxT'])
 
-    def run_(self, MaxT = None):
+    def run(self, MaxT = None):
         while True:
-            for iteration in self.model.make_step(maxT = MaxT):
+            for iteration in self.model.make_step(maxT = self.maxT):
                 print(iteration['step'])
                 if iteration['step']>50:
                     self.model.gx_0 = 0
