Add seed setting and fix stress rotation formula

model.py
@@ -52,9 +52,12 @@ class Model(object):
 
         self.__dict__.update(defaults)
         self.__dict__.update(self.markers)
-        self.__dict__.update(parameters.settings)
 
         for key in self.__dict__:
+            try:
+                self.__dict__[key] = parameters[key]
+            except KeyError:
+                pass
             if self.__dict__[key] is None:
                 raise ValueError (f'{key} parameter must be set')
 
@@ -191,6 +194,12 @@ class Model(object):
                             Vbound[(i,j)] = [Vx,Vy]
                         print(Vbound)
 
+                        eta_s_ = np.copy(eta_s)
+                        eta_n_ = np.copy(eta_n)
+                        rho_ = np.copy(rho)
+                        so_xx_ = np.copy(so_xx)
+                        so_xy_ = np.copy(so_xy)
+
                         Stokes_sparse, vector = return_sparse_matrix_Stokes(j_res, i_res, dx, dy,
                                         eta_s, eta_n, rho, gx_0, gy_0, so_xx, so_xy, kbond, kcont, p0cell,
                                         lower_boundary=self.bottom_bound, upper_boundary=self.top_bound,
@@ -250,7 +259,7 @@ class Model(object):
                 m_P  = interpolate2m(mxx-.5, myy-.5, P[1:,1:]) # tecnichaly, there must be +.5,+.5
                                                                # but since we slice P, indexing goes one item lower
 
-                w = dVy_dx - dVx_dy
+                w = 0.5*(dVy_dx - dVx_dy)
 
                 m_s_xx, m_s_xy = self.interpolate_stress_changes(mxx, myy,
                                                                  m_s_xx, m_s_xy,
@@ -264,27 +273,21 @@ class Model(object):
                 m_w    = interpolate2m(mxx-.5 ,myy-.5 , w)
 
                 m_a = m_w * dt
-                m_s_xx_ = m_s_xx - m_s_xy * 2 * m_a
-                m_s_xy_ = m_s_xy + m_s_xy * 2 * m_a
-                m_s_xx, m_s_xy = m_s_xx_, m_s_xy_
+
+                # m_s_xx_ = m_s_xx - m_s_xy * 2 * m_a
+                # m_s_xy_ = m_s_xy + m_s_xx * 2 * m_a
+                # m_s_xx, m_s_xy = m_s_xx_, m_s_xy_
+
+                m_s_xx_ = m_s_xx*(np.cos(m_a)**2 - np.sin(m_a)**2 ) - m_s_xy*np.sin(2*m_a)
+                m_s_xy_ = m_s_xx*np.sin(2*m_a) + m_s_xy*np.cos(2*m_a)
+
+
+
 
                 self.advect(mxx, myy, m_Vx, m_Vy, Vx, Vy, dt)
 
                 T += dt
                 step +=1
-
-                #s_xx = 2 * eta[1:,1:] * e_xx
-
-                #d_s_xx = s_xx - average(sxx_0)
-
-                #s_xy = 2 * eta * e_xy
-
-                #d_s_xy = s_xy - sxy_0
-
-                #sii = (s_xx**2 + average(s_xy)**2)**.5
-                #eii = (e_xx**2 + average(e_xy)**2)**.5
-
-                #if step % step : continue
 
                 parameters = {'T' : T,
                               'step' : step,
@@ -308,6 +311,11 @@ class Model(object):
                               'w' : w,
                               'markers_index_list' : self.markers_index_list,
                               'moving_points_index_list' : self.moving_points_index_list,
+                              'eta_s_' : eta_s_,
+                              'eta_n_' : eta_n_,
+                              'rho_' : rho_,
+                              'so_xx_' : so_xx_,
+                              'so_xy_' : so_xy_,
                              }
                 yield parameters
 
@@ -316,11 +324,13 @@ class Model(object):
             m_s_xx = interpolate2m(mxx-.5,myy-.5,s_xx)
             m_s_xy = interpolate2m(mxx,myy,s_xy)
         elif self.stress_changes == '2nd order':
+            print('2nd order')
             m_ds_xx = interpolate2m(mxx-.5,myy-.5,ds_xx)
             m_ds_xy = interpolate2m(mxx,myy,ds_xy)
             m_s_xx = m_s_xx + m_ds_xx
             m_s_xy = m_s_xy + m_ds_xy
         elif self.stress_changes == 'subgrid':
+            print('subgrid')
             d_ve = self.subgrid_relaxation
             m_eta = self.m_eta
             m_mu = self.m_mu

view.py
@@ -73,6 +73,30 @@ class Matplot(object):
                                              parameters['T']))
         plt.close(fig)
 
+    def plotStokes(self, parameters):
+        """ Create plot of grides used to produce Stokes matrix"""
+        plt.clf()
+        fig = plt.figure(figsize = (30,20))
+
+        things_to_plot = ('eta_s_', 'eta_n_', 'rho_', 'so_xx_', 'so_xy_')
+        titles =  (r'$\eta_s$', r'$\eta_n$', r'$\rho$', r'$\sigma_{xx}$', r'$\sigma_{xy}$')
+
+        for i,(array, title) in enumerate(zip(things_to_plot, titles)):
+            i+=1
+            normalize = False
+            if array in ('so_xx_','so_xy_'):
+                normalize =  False
+            plt.subplot(2,3,i)
+            self._plot_simple_w_colorbar(parameters, array, title=title, normalize=normalize)
+
+        plt.subplot(2,3,6)
+        self._plot_particals(parameters)
+
+
+        plt.savefig('%s/matrix_%003d-%s.png' % (self.figname,
+                                             parameters['step'],
+                                             parameters['T']))
+        plt.close(fig)
 
     def plot12(self, parameters):
         """ Make 12 plots on a list """
@@ -161,16 +185,27 @@ class Matplot(object):
         plt.xlim([0,self.j_res-2])
 
 
-    def _plot_simple_w_colorbar(self, parameter, array, title=None, cmap='seismic', fontsize=30):
+    def _plot_simple_w_colorbar(self, parameter, array, title=None, cmap='seismic', fontsize=30, normalize=False):
         if not title:
             title=array
         plt.title(title, fontsize=fontsize)
         A = parameter[array]
+        max_ = float(max(np.max(A), np.abs(np.min(A))))
+
         try:
             slicer = self.slices[array]
         except KeyError:
             slicer = self.slices['default']
-        plt.imshow(slicer(A),
-                   interpolation='none',
-                   cmap=cmap)
+        if normalize:
+            plt.imshow(slicer(A),
+                       interpolation='none',
+                       cmap=cmap,
+                       vmin = -1*max_,
+                       vmax = max_,
+                       )
+        else:
+            plt.imshow(slicer(A),
+                       interpolation='none',
+                       cmap=cmap,
+                       )
         plt.colorbar()